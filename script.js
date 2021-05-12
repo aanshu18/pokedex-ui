113c54a647fd8f9fd73e3b5678ed1fbc4fb59226
anshu369 <ashutosh3194@gmail.com>
2021-05-12 15:27:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 let pokemonUrl = `https://pokeapi.co/api/v2/pokemon/`;
 let pokemonImageUrl = "https://pokeres.bastionbot.org/images/pokemon/";
-const pokemonCount = 210;
+const pokemonCount = 5;
+
+const colors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5'
+};
+
 
 let pokemonWindow = document.getElementById("poke-id");
 
@@ -27,11 +45,9 @@ function createPokemonCard(pokemon) {
 
     let pokeCard = document.createElement('div');
     pokeCard.className = "pokeClass";
-    let innerHtml = `${pokemon.name.toUpperCase()}`;
+    let pokeImageUrl = pokemonImageUrl + pokemon.id + ".png";
+    console.log(pokeImageUrl);
+    let innerHtml = `<img class="pokeImage" src=${pokeImageUrl}><small class="pokeId">#${pokemon.id}</small><h3 class="pokeText">${pokemon.name[0].toUpperCase()+pokemon.name.slice(1)}</h3><h5>${pokemon.types[0].type.name}</h5>`;
     pokeCard.innerHTML = innerHtml;
-    let pokeData = document.createElement('div');
-    pokeData.className = "pokeText";
-    pokeData.innerHTML = pokemon.id;
-    pokeCard.appendChild(pokeData);
     pokemonWindow.appendChild(pokeCard);
 }
